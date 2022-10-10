4003328943e082a9ff52743b9fb115a1bed3dafc
Darshan Gowda G <108842799+D2Gowda@users.noreply.github.com>
2022-10-10 21:29:58 +0530

    DV

---
+++ Example1.sql
@@ -0,0 +1,3 @@
+/* 1) create table product with fields(product_id, product_name, date_of_manufacture, expiry_date, marketed_by, unit_price). Make product_id as primary key.
+   2) create table order eith fields()
+*/
