6ea4112bf1a0b2dcaf860e2234b1069a7bb6def9
Darshan Gowda G <108842799+D2Gowda@users.noreply.github.com>
2022-10-11 20:02:52 +0530

    DV

--- Example1.sql
+++ Example1.sql
@@ -1,3 +1,9 @@
-/* 1) create table product with fields(product_id, product_name, date_of_manufacture, expiry_date, marketed_by, unit_price). Make product_id as primary key.
-   2) create table order eith fields()
+/* 1) create table product with fields(product id, product name, date of manufacture, expiry date, marketed by, unit price). Make product_id as primary key.
+   2) create table order with fields(order id, product id, quantity, price). Make order id as primary key and quantity should be more than 0.
+   3) Display the structure of fields.
+   4) Add a field delivery date in order table.
+   5)
+   6)
+   7)
+   8) 
 */
