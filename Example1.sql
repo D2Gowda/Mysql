ca1b158a41c7e1449eeaa889db0969c5322c723c
Darshan Gowda G <108842799+D2Gowda@users.noreply.github.com>
2022-10-11 21:04:06 +0530

    DV

--- Example1.sql
+++ Example1.sql
@@ -2,8 +2,11 @@
    2) create table order with fields(order id, product id, quantity, price). Make order id as primary key and quantity should be more than 0.
    3) Display the structure of fields.
    4) Add a field delivery date in order table.
-   5)
-   6)
-   7)
-   8) 
+   5) change the name of field of dat of manufacture to DOM.
+   6) Insert atleast 5 records in each table.
+   7) The order table should contain the products that are already in product table.
+   8) Display the records in table.
+   9) Display the marketed by names with duplicates.
+   10) Modify the price field that the amount should not be zero.
+   11) 
 */
